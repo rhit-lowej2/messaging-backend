80bcdb456ab8dc8bf827e4d8ca83eb382305c9b1
Joshua Lowe <lowej2@rose-hulman.edu>
2025-03-26 23:19:48 +0000

    set up login api

--- app.py
+++ app.py
@@ -1,8 +1,25 @@
 
 # using flask_restful
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, request, make_response
 from flask_restful import Resource, Api
-from flask_cors import CORS, cross_origin
+from flask_cors import CORS
+from pymongo import MongoClient
+from pymongo.server_api import ServerApi
+import bcrypt
+
+uri = "mongodb+srv://admin:cq3Nbw6BFbU7GPcR@private-messaging.hk8sb.mongodb.net/?retryWrites=true&w=majority&appName=private-messaging"
+
+# Create a new client and connect to the server
+client = MongoClient(uri, server_api=ServerApi('1'))
+# Send a ping to confirm a successful connection
+try:
+    client.admin.command('ping')
+    print("Pinged your deployment. You successfully connected to MongoDB!")
+except Exception as e:
+    print(e)
+
+auth = client['auth']
+
 
 # creating the flask app 
 app = Flask(__name__)
@@ -14,21 +31,37 @@ api = Api(app)
 # the get, post methods correspond to get and post requests 
 # they are automatically mapped by flask_restful. 
 # other methods include put, delete, etc. 
-class Hello(Resource):
-
-    # corresponds to the GET request. 
-    # this function is called whenever there 
-    # is a GET request for this resource 
-    def get(self):
-
-        return jsonify({'message': 'hello world'})
-
-        # Corresponds to POST request
+class Login(Resource):
     def post(self):
+        data = request.get_json()
 
-        data = request.get_json()     # status code 
-        return jsonify({'data': data}), 201
+        user = data.get('username')
+        password = data.get('password').encode('utf-8')
 
+        if user and password:
+            out = auth.users.find_one({'username': user})
+            if out and bcrypt.checkpw(password, out['password']):
+                return make_response(jsonify({'login': True}), 200)
+
+        return make_response(jsonify({'error': "Failed to login"}), 409)
+
+
+class Signup(Resource):
+    def post(self):
+        data = request.get_json()
+
+        if not data or 'username' not in data or 'password' not in data:
+            return make_response(jsonify({"error": "Missing username or password"}), 400)
+
+        username = data['username']
+        password = data['password'].encode('utf-8')
+
+        if auth.users.find_one({'username': username}):
+            return make_response(jsonify({"error": "User already exists"}), 409)
+
+        auth.users.insert_one({'username': username, 'password': bcrypt.hashpw(password, bcrypt.gensalt())})
+
+        return make_response(jsonify({"message": "User created successfully"}), 201)
 
 # another resource to calculate the square of a number 
 class Square(Resource):
@@ -39,8 +72,9 @@ class Square(Resource):
 
 
     # adding the defined resources along with their corresponding urls
-api.add_resource(Hello, '/')
+api.add_resource(Login, '/login')
 api.add_resource(Square, '/square/<int:num>')
+api.add_resource(Signup, '/signup')
 
 
 # driver function 
